57945ec3bca10d3f76122c89e339b920dde666d0
jscbridge <jorge.sanchez@thebridgeschool.es>
2022-08-18 15:04:19 +0200

    Con temporizador

--- client/src/component/Acertar.js
+++ client/src/component/Acertar.js
@@ -4,7 +4,7 @@ function Acertar() {
   const [intervaloI, setIntervaloI] = useState(0);
   const [intervaloD, setIntervaloD] = useState(999);
   const [count, setCount] = useState(0);
-
+  const [second, setSecond] = useState(5);
   const [secreto, setSecreto] = useState();
   const [valor, setValor] = useState();
 
@@ -13,12 +13,25 @@ function Acertar() {
     setSecreto(Math.floor(Math.random() * 1000));
   }, []);
 
-  
-
   //Las comprobaciones
   function comprobar(e) {
     if (secreto == e) {
-     alert("OLEEE!!! Tardaste:" + count + " intentos");
+      setSecreto("end");
+      setIntervaloI(0);
+      setIntervaloD(100);
+
+      //   setTimeout(() => {
+      //     window.location.reload();
+      //   }, 4000);
+      var n = 5;
+      window.setInterval(function () {
+        n--;
+        setSecond(n)
+        if (n == 0) {
+            window.location.reload();
+            setSecond(5);
+        }
+      }, 1000);
     } else if (e < secreto) {
       setCount(count + 1);
       setIntervaloI(e);
@@ -45,6 +58,8 @@ function Acertar() {
         onChange={(e) => setValor(e.target.value)}
       ></input>
       <button onClick={() => comprobar(valor)}>Comprobar</button>
+      {secreto == "end" ? <p>Acertaste al tardaste al {count} intentos</p> : ""}
+      {secreto == "end" ? <p>Se reiniciará en {second} segundos</p> : ""}
     </div>
   );
 }
